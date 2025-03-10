606a8b678414240883bc69289e54d1f8c65c8797
mjhdu13 <mjhdu13@gmail.com>
2025-03-10 17:58:29 +0600

    interactivity of 6 boxes is almost okay

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,6 +1,38 @@
-let buttons = document.getElementsByClassName('completed');
-for(let i = 0; i < buttons.length; i++ ){
-  buttons[i].addEventListener('click', function(){
+const sixBoxes = document.getElementById('six-boxes');
+const history = document.getElementById('history');
+
+const boxDivs = sixBoxes.getElementsByTagName('div'); 
+
+const getCountDiv = document.getElementById('task-count-number'); 
+const getCountDivH = document.getElementById('big-number');
+
+// const buttons = document.querySelectorAll('.btn')
+// console.log(buttons);
+// console.log(boxDivs);
+
+for (let singleBox of boxDivs){
+  const btn = singleBox.querySelector('button')
+  btn.addEventListener('click', function() {
+    const activityLog =document.createElement('p');
+    const divText = singleBox.querySelector('h1').textContent;
+    activityLog.innerText = 'you have selected ' + divText + '(Ekhane click korar time ta bosbe)'; 
+    // uporer line e time er formula bosalei kaj ok.
     alert('Board Updated Successfully')
-  })
+    history.append(activityLog);
+    // notification gulor bg color and margin + padding dite hobe
+    // activityLog.style.backgroundColor=('yellow')
+    btn.setAttribute('disabled', true)
+
+    let currentCount = parseInt(getCountDiv.innerText); 
+    let updatedCount = currentCount - 1 ; 
+    getCountDiv.innerText = updatedCount;
+
+    let currentCountH = parseInt(getCountDivH.innerText); 
+    let updatedCountH = currentCountH + 1 ; 
+    getCountDivH.innerText = updatedCountH;
+  });
 }
+
+// if(currentCount === 0){
+    //   alert('YEEEESSSSS')
+    // }
